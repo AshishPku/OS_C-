b9a840adb35c96cbe6d82d35b430d162f06d385f
AshishPku <2022uec1698@mnit.ac.in>
2024-06-11 10:44:24 +0530

    This program will create a file and write some text,code,etc into the file and close it so that there is no any unnecessay memory usages.

---
+++ WritingIntoFile.cpp
@@ -0,0 +1,12 @@
+//cerr in c++ is used to show error messages like cout to show messages.
+//fstream is a library in c++ which is used to create, write or read files.
+//ofstream 	Creates and writes to files
+//ifstream 	Reads from files
+//fstream 	A combination of ofstream and ifstream: creates, reads, and writes to files
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ofstream coutf("File1.txt");
+    coutf<<"This is first program written by c++ directly into the file system ";
+    coutf.close();
+}
